d1b7d8be024268570b98a7c33279b16b533c8719
Rokoucha <git@rokoucha.net>
2023-07-31 02:33:16 +0900

    fix storybook vanilla-extract integration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,8 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-const config: StorybookConfig = {
+export default {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
     '@storybook/addon-links',
@@ -15,5 +17,33 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
-}
-export default config
+  webpackFinal(config, options) {
+    config.plugins?.push(new VanillaExtractPlugin(), new MiniCssExtractPlugin())
+
+    config.module?.rules?.forEach((rule) => {
+      if (
+        typeof rule === 'object' &&
+        rule !== null &&
+        rule.test instanceof RegExp &&
+        rule.test.test('test.css')
+      ) {
+        rule.exclude = /\.vanilla\.css$/i
+      }
+    })
+
+    config.module?.rules?.push({
+      test: /\.vanilla\.css$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            url: false,
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+} satisfies StorybookConfig
