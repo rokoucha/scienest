f3f09957f10941f0444361c87f51b9d9d3893d28
Rokoucha <git@rokoucha.net>
2023-08-14 05:16:28 +0900

    bundle analyze

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
+import createBundleAnalyzer from '@next/bundle-analyzer'
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true,
+    swcMinify: true,
     typedRoutes: true,
   },
   reactStrictMode: true,
 }
 
-export default withVanillaExtract(nextConfig)
+export default withBundleAnalyzer(withVanillaExtract(nextConfig))
