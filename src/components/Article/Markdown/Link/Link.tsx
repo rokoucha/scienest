1e41294f1af806301376dacfde84f2b2b312c2ab
Rokoucha <git@rokoucha.net>
2024-01-09 14:45:13 +0900

    リンクが壊れるパターンがあったので様子見

--- src/components/Article/Markdown/Link/Link.tsx
+++ src/components/Article/Markdown/Link/Link.tsx
@@ -8,25 +8,31 @@ import { buttonLikeLink, linkIcon, linkText, wrapper } from './Link.css'
 export function Link<RouteType>({
   children,
   className,
+  href,
   ...props
 }: LinkProps<RouteType>): React.ReactNode {
   const isExternal = useMemo<boolean>(() => {
-    if (props.href === undefined) return false
+    if (href === undefined) return false
 
     try {
-      new URL(String(props.href))
+      new URL(String(href))
       return true
     } catch {
       return false
     }
-  }, [props.href])
+  }, [href])
 
   const isButtonLike =
     !isExternal && children && String(children).startsWith('#')
 
+  const encodedHref = useMemo<URL>(() => {
+    return new URL(String(href), process.env.BASE_URL)
+  }, [href])
+
   return (
     <NextLink
       className={clsx(className, wrapper, isButtonLike && buttonLikeLink)}
+      href={encodedHref}
       {...(isExternal && { target: '_blank', rel: 'noopener noreferrer' })}
       {...props}
     >
