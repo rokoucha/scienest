132a7e5245358d84ee9f8c70e0cf2e61e364d1e7
Rokoucha <git@rokoucha.net>
2024-01-26 00:02:55 +0900

    Twitter card の読み込み中の見た目を変える

--- src/components/TwitterCard/TwitterCard.css.ts
+++ src/components/TwitterCard/TwitterCard.css.ts
@@ -1,13 +1,13 @@
 import { style } from '@vanilla-extract/css'
 
 export const wrapper = style({
-  borderColor: 'grey',
-  borderRadius: '0.5rem',
+  borderColor: 'dodgerblue',
+  borderRadius: '3px',
   borderStyle: 'solid',
   borderWidth: '1px',
   color: 'dodgerblue',
   margin: 0,
-  padding: '0.5rem',
+  padding: '0.25rem',
   textDecoration: 'underline',
   width: 'max-content',
 })
