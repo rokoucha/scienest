fa37c3946d63a8736ef1e3a904bf2aa2e20701fe
Rokoucha <git@rokoucha.net>
2024-08-16 22:30:07 +0900

    drizzleのログ吐く

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,4 +2,4 @@ import { DrizzleD1Database, drizzle } from 'drizzle-orm/d1'
 import * as schema from './schema'
 
 export type Database = DrizzleD1Database<typeof schema>
-export const db = drizzle(process.env.DB, { schema })
+export const db = drizzle(process.env.DB, { schema, logger: true })
