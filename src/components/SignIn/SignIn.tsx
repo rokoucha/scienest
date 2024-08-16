9ad7ff0ad4262e2d24092943a574b69c55add89d
Rokoucha <git@rokoucha.net>
2024-08-16 23:03:42 +0900

    signin/signoutなおす

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { signIn } from '../../auth'
 import { Button } from '../Button'
 
 export const SignIn: React.FC = () => {
   return (
-    <form action="/auth/signin/github" method="post">
+    <form
+      action={async () => {
+        'use server'
+        await signIn('github')
+      }}
+      method="post"
+    >
       <Button>Sign In</Button>
     </form>
   )
--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { signOut } from '../../auth'
 import { Button } from '../Button'
 
 export const SignOut: React.FC = () => {
   return (
-    <form action="/auth/signout" method="get">
+    <form
+      action={async () => {
+        'use server'
+        await signOut()
+      }}
+    >
       <Button>Sign Out</Button>
     </form>
   )
